4d95ad9b2af27ca5b6b2d72851c2828fad2ed501
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-11-05 20:25:40 -0300

    adicionando arquivos

---
+++ funcoes/MANIPULANDO_STRINGS/index.php
@@ -0,0 +1,21 @@
+<?php
+//  são funções que trabalham sobre strings para que sejam efetuado tarefas (como as funções)
+
+strtolower($texto); // Transforma todos os caracteres em minusculos
+
+
+strtoupper($texto); //  trasnforma todos os caracteres da string em maiusculo
+
+ucfirst($texto);  // trasnforma o primeiro caracter da string em maiusculo
+
+
+strlen($texto); // conta a quantidade de carateres de uma string
+
+
+str_replace('A', 'a', $texto); // substutitu uma cadeia de caracteres por outra dentro de uma string
+
+
+substr($texto, '', '');  // retorna parte de uma string
+
+
+
---
+++ funcoes/funcao.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>funções</title>
+</head>
+
+<body>
+    <?php
+    // encapsular o bloco de códigos com um objetivos definido
+    /*
+     *os parametros são utilizados como entrada de dados para função
+    * lembrar de sempre chamar a função
+    */
+    function exibirBoasVindas()
+    {
+        echo 'Bem vindo ao curso de PHP';
+    }
+
+    exibirBoasVindas();
+
+    function calcularArea($largura, $comprimento)
+    {
+        $area =    $largura * $comprimento;
+        return $area;
+    }
+    echo '<br>';
+    echo calcularArea(30, 50); // ,quando se chama uma função deve-se passar parametros dentro dela
+    echo '<br>';
+    echo   calcularArea(30, 22); // posso chamar quantas vezes forem necessário as funções
+    echo '<br>';
+    $retorno = calcularArea(30, 88); // existe duas manerias de se dar retorno em uma função, por meio de echo ou por meio de uma variavel PHP como esta em questão
+    echo $retorno;
+
+    /* QUANDO DEVO UTILIZAR OS RETORNOS COM OU SEM VARIAVEL DE RETORNO
+    
+    SEM VARIAVEL DE RETORNO --> 
+
+    */
+
+    ?>
+</body>
+
+</html>
