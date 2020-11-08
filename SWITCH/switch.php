266bdd8d1c7c7c0c0ac8952f84f77c9376002164
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-11-08 11:44:09 -0300

    incluindo novos arquivos

---
+++ SWITCH/switch.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch</title>
+</head>
+
+<body>
+    <?php
+    $parametro = 2;
+    switch ($parametro) {
+        case 1:
+
+            echo 'entrou no case 1';
+            break;
+
+        case 2:
+            echo 'Entrou no case 2';
+            break;
+
+        case 3:
+            echo 'entoru no case 3';
+            break;
+        default:
+
+            break;
+    }
+
+    ?>
+</body>
+
+</html>
