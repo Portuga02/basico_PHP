249329336abb4cbb76ad1ac6c8561daf7bf38942
=Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-04-29 22:40:48 -0300

    atualizando

--- While/index.php
+++ While/index.php
@@ -1,14 +1,10 @@
- <!--Comando While para condições e loops */ -->
- <?php
+<!--Comando While para condições e loops */ -->
+<?php
+$numero = 0;
 
-    $numero  = 0;
+while ($numero < 10) {
+    echo "o numero é :" . $numero . "<br>";
 
-    while ($numero  < 10) {
-        echo "o numero é :" . $numero . "<br>";
-
-
-        $numero += 1; /*formas de concatenação e adição de um numero para não dar loop infinito*/
-    }
-
-
-    ?>
+    $numero += 1; /* formas de concatenação e adição de um numero para não dar loop infinito */
+}
+?>
