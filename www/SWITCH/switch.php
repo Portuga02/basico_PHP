18e739fe3105f5c73a53ae3f8e8a7f87d50b9e31
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-01-20 21:41:38 -0300

    novas formas de fazer swith case

--- www/SWITCH/switch.php
+++ www/SWITCH/switch.php
@@ -9,6 +9,16 @@
 
 <body>
     <?php
+    class thami
+    {
+        public $arrayDados = [1, 2, 3, 4];
+
+
+        public function funcionandoTamy()
+        {
+            $this->arrayDados;
+        }
+    }
     $parametro = 2;
     switch ($parametro) {
         case 1:
@@ -25,6 +35,53 @@
             break;
         default:
 
+            break;
+    }
+    // segunda forma de se passar um case por string
+    
+    $parametro = "1";
+    switch ($parametro) {
+        case "1":
+
+            echo 'entrou no case 1';
+            break;
+
+        case "2":
+            echo 'Entrou no case 2';
+            break;
+
+        case "3":
+            echo 'entoru no case 3';
+            break;
+        default:
+
+            break;
+    }
+
+
+    // terceira forma de se passa um valor no case, por variavel, pode-se usar também metoods para que seja feito calculos
+    
+    $tam = $tam->funcionandoTamy;
+    switch ($tam) {
+        case ($tam == 1):
+
+            echo $tam . "1";
+            break;
+
+        default:
+            # code...
+            break;
+    }
+
+    // quarta forma de se passar dados, chamando diretamente o metodo da classe
+    switch ($tam->funcionandoTamy) {
+        case ($tam == 1):
+
+            return $tam;
+            break;
+
+        default:
+            # code...
             break;
     }
 
