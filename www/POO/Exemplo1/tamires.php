67a5edcf20231a6557f34e0a3637dce24ed974d8
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-01-20 21:36:14 -0300

    implementando POO e switch case com poo

---
+++ www/POO/Exemplo1/tamires.php
@@ -0,0 +1,29 @@
+<?php
+class thami
+{
+    public $arrayDados = [1,2,3, 4];
+
+
+    public function funcionandoTamy()
+    {
+        $this->arrayDados;
+    }
+}
+
+$thamires = $thami = new thami();
+
+
+$tam = $thami->funcionandoTamy();
+
+
+
+switch ($tam) {
+    case ($tam == 1):
+
+        echo $tam . "1";
+        break;
+
+    default:
+        # code...
+        break;
+}
