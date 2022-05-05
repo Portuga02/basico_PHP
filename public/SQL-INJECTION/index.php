547c476d462a94e7bfdd7b6a10c474cca0a87624
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-05-05 20:55:23 -0300

    estudando encapsulamento

--- public/SQL-INJECTION/index.php
+++ public/SQL-INJECTION/index.php
@@ -1,29 +1,29 @@
 <?php
 
-if (!empty($_POST['usuario']) && !empty($_POST['senha'])) {
+if ( !empty( $_POST[ 'usuario' ] ) && !empty( $_POST[ 'senha' ] ) ) {
 
     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuario = 'root';
     $senha = '';
 
     try {
-        $conexao = new PDO($dsn, $usuario, $senha);
+        $conexao = new PDO( $dsn, $usuario, $senha );
 
-        $query = "select * from tb_usuarios where";
+        $query = 'SELECT * from tb_usuarios where';
         $query .= " email = '{$_POST['usuario']}' ";
         $query .= " AND senha = '{$_POST['senha']}'";
 
         echo $query;
 
-        $stmt = $conexao->query($query);
+        $stmt = $conexao->query( $query );
 
         $usuario = $stmt->fetch();
         echo '<hr>';
 
         echo '<pre>';
-        print_r($usuario);
+        print_r( $usuario );
         echo '</pre>';
-    } catch (PDOException $e) {
+    } catch ( PDOException $e ) {
         echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
         //registrar o erro de alguma forma.
     }
@@ -31,19 +31,17 @@ if (!empty($_POST['usuario']) && !empty($_POST['senha'])) {
 ?>
 
 <html>
-<meta charset="utf-8">
+<meta charset = 'utf-8'>
 <title>Login</title>
 
 </html>
 
-
-
 <body>
-    <form method="post" action="index.php">
-        <input type="text" placeholder="usuário" name="usuario" />
-        <br />
-        <input type="password" placeholder="senha" name="senha" />
-        <br />
-        <button type="submit">Entrar</button>
-    </form>
+<form method = 'post' action = 'index.php'>
+<input type = 'text' placeholder = 'usuário' name = 'usuario' />
+<br />
+<input type = 'password' placeholder = 'senha' name = 'senha' />
+<br />
+<button type = 'submit'>Entrar</button>
+</form>
 </body>
--- public/foreach/index.php
+++ public/foreach/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h1> EXEMPLOS PARA INGREDIENTES </h1>";
+echo '<h1> EXEMPLOS PARA INGREDIENTES </h1>';
 $ingredientes = [
     'feijao',
     'carne',
@@ -9,25 +9,26 @@ $ingredientes = [
 ];
 
 /*forma simples do foreach  adicionando o valor do array a variavel ingrediente*/
-foreach ($ingredientes as $ingrediente) {
+foreach ( $ingredientes as $ingrediente ) {
 
-    echo "Item's  " . $ingrediente . "<br>";
+    echo "Item's  " . $ingrediente . '<br>';
 }
-echo "<pre>";
-var_dump($ingredientes);
-echo "</pre>";
+echo '<pre>';
+var_dump( $ingredientes );
+echo '</pre>';
 ?>
 
 <?php
-echo "" . "<br>";
+echo '' . '<br>';
 /*só criei aqui para dar um espacinho*/
 
 ?>
 
 <?php
 /*nesse tipo de foreach ele pega os dados e coloca em uma chave que é a posição do array*/
-/*nesse caso então teremos acesso a chave [0] (a chave encontra-se assim); e o valor na posição */
-echo "<h1> EXEMPLOS PARA DADOS </h1>";
+/*nesse caso então teremos acesso a chave [ 0 ] ( a chave encontra-se assim );
+e o valor na posição */
+echo '<h1> EXEMPLOS PARA DADOS </h1>';
 $dados = [
     'access',
     'sql-server',
@@ -36,17 +37,16 @@ $dados = [
     'sql-lite'
 ];
 
-
-foreach ($dados as $key => $dado) {
-    echo "Item " . $key . ":" . $dado . "<br>";
+foreach ( $dados as $key => $dado ) {
+    echo 'Item ' . $key . ':' . $dado . '<br>';
 }
-echo "<pre>";
-var_dump($dados);
-echo "</pre>";
+echo '<pre>';
+var_dump( $dados );
+echo '</pre>';
 ?>
 
 <?php
-echo "<h1> EXEMPLOS PARA SISTEMAS OPERACIONAIS </h1>";
+echo '<h1> EXEMPLOS PARA SISTEMAS OPERACIONAIS </h1>';
 
 $SO = [
     'Windows',
@@ -57,19 +57,17 @@ $SO = [
 ];
 
 /* CASO SE TENHYA NECESSIDADE DE COMÇER COM O NUMERO 1 EM VEZ DO ZERO
-*   IMPLEMTENTA O ( +1) concatenando com a variavel $key
+*   IMPLEMTENTA O ( +1 ) concatenando com a variavel $key
 
 */
-foreach ($SO as $key => $listaSO) {
-    echo "Item " . ($key + 1) . ":" . $listaSO . "<br>";
+foreach ( $SO as $key => $listaSO ) {
+    echo 'Item ' . ( $key + 1 ) . ':' . $listaSO . '<br>';
 }
-echo "<pre>";
-var_dump($SO);
-echo "</pre>";
+echo '<pre>';
+var_dump( $SO );
+echo '</pre>';
 ?>
 
-
-
-<!-- LAÇO FOREACH  
+<!-- LAÇO FOREACH
 
 * UTILIZADO  PARA PERCORRER ARRAYS DE ITENS -->
