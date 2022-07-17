8621edf09cdd3516d722a84616f8da5019c67fee
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-07-16 21:24:37 -0300

    Configurando e estudando metodos staticos

---
+++ public/POO/FUNCOES_STATICAS/classMatematica.php
@@ -0,0 +1,30 @@
+<?php
+class matematica
+{
+    public static int $int = 4;
+
+    public static string $nome = 'Estudando metodos staticos';
+
+    public static function somar($x, $y, $z)
+   
+    {
+        return $x + $y + $z;
+    }
+
+    public  static function dividir($x, $y, $z)
+    {
+        return $x / $y / $z;
+    }
+
+    public static function multiplicar($x, $y, $z)
+    {
+        return $x * $y * $z;
+    }
+
+    public static function subtrair($x, $y, $z)
+    {
+        return $x * $y * $z;
+    }
+
+    
+}
