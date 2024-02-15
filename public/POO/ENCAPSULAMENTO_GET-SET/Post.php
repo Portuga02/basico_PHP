bce53a277d30f351713af02831824f4596485670
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-02-15 14:49:08 -0300

    PEQUENO IDENT

--- public/POO/ENCAPSULAMENTO_GET-SET/Post.php
+++ public/POO/ENCAPSULAMENTO_GET-SET/Post.php
@@ -1,32 +1,34 @@
 <?php
 
-class Post {
-
+class Post
+{
     public int $id;
     public int $likes = 0;
-    private  string $author;
+    private string $author;
 
-    public function aumentarLikes() {
+    public function aumentarLikes()
+    {
         $this->likes++;
     }
 
-    public function setAuthor( $n ) {
-        $this->author = ucfirst( $n );
+    public function setAuthor($n)
+    {
+        $this->author = ucfirst($n);
     }
 
-    public function getAuthor() {
+    public function getAuthor()
+    {
         return $this->author;
     }
 
 }
 $post1 = new Post();
 
-$post1->setAuthor( 'Sávio' );
+$post1->setAuthor('Sávio');
 
 $post2 = new Post();
-$post2->setAuthor( 'Gomes' );
+$post2->setAuthor('Gomes');
 
-echo 'POST1'.$post1->likes.'likes -'.$post1->getAuthor().'<br>';
+echo 'POST1' . $post1->likes . 'likes -' . $post1->getAuthor() . '<br>';
 
-echo 'POST2'.$post2->likes.'likes -'.$post1->getAuthor().'<br>';
-?>
+echo 'POST2' . $post2->likes . 'likes -' . $post1->getAuthor() . '<br>';
